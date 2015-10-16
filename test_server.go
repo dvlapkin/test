157edfeb38676352b5751b6f4b8749f8a8fa45f5
dvlapkin <lapkin@inbox.ru>
2015-10-16 13:09:16 +0400

    upd +struct print to io.W , - bug with Duration

--- test_server.go
+++ test_server.go
@@ -4,34 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"io"
 	"time"
 	"reflect"
 	"strings"
 	"strconv"
+	"os"
 )
 type MyForm struct{
-		UserName 	string `required:"true" field:"name" name:"Имя пользователя" type:"text" default:"true"`
-		Child int `field:"child" name:"Дети" type:"text"`
-		Age int64 `field:"age" name:"Возраст" type:"text" default:"true"`
-		UserPassword 	string `required:"true" field:"password" name:"Пароль пользователя" type:"password"`
-		Gender	 	string `required:"true" field:"gerder" name:"Пол" type:"select" select:"Не	известный=3,Мужской=1;selected,Женский=2"`
-		Resident bool `field:"resident" type:"radio" radio:"1;checked" name:"Резидент РФ"`
-		NoResident bool `field:"resident" type:"radio" radio:"2" name:"Не резидент РФ"`
-		Foot bool `field:"body" type:"radio" radio:"1;checked" name:"Нога"`
-		Hand bool `field:"body" type:"radio" radio:"2" name:"Рука"`
-		Comment 	string `required:"true" field:"tarea" name:"комментарий" type:"textarea"`
-
+	UserName 	string `required:"true" field:"name" name:"Имя пользователя" type:"text" default:"true"`
+	Login 		int `field:"login" name:"Логин" type:"text"`
+	Age 		int64 `field:"age" name:"Возраст" type:"text" default:"true"`
+	Koe 		float64 `field:"koef" name:"Коэфицент" type:"text" default:"true"`
+	UserPassword 	string `required:"true" field:"password" name:"Пароль пользователя" type:"password"`
+	Gender	 	string `required:"true" field:"gerder" name:"Пол" type:"select" select:"Не	известный=3,Мужской=1;selected,Женский=2"`
+	Resident 	bool `field:"resident" type:"radio" radio:"1;checked" name:"Резидент РФ"`
+	NoResident 	bool `field:"resident" type:"radio" radio:"2" name:"Не резидент РФ"`		
+	Foot 		bool `field:"body" type:"radio" radio:"1;checked" name:"Нога"`
+	Hand 		bool `field:"body" type:"radio" radio:"2" name:"Рука"`
+	Duration 	time.Duration `required:"true" field:"duration" name:"Длительность" type:"text" default:"true"`
+	Comment 	string `required:"true" field:"tarea" name:"комментарий" type:"textarea"`
 }
 	
 
-
-var (myform *MyForm
+var (
+	myform *MyForm
 	err		error
 	)
 	
 
 func main() {
-	myform = &MyForm{UserName:"user",Age:21}
+	myform = &MyForm{UserName:"user",Age:21,Koe:2.34,Duration:12345678}
 	fmt.Println(myform)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			
@@ -40,7 +43,7 @@ func main() {
 				if err != nil {
 					fmt.Println(err)
 					}						
-				fmt.Fprint(w,frm_crt)			
+				fmt.Fprint(w,frm_crt)					
 				}
 				
 			if r.Method == "POST" {
@@ -48,7 +51,8 @@ func main() {
 				if err != nil {
 					log.Fatal(err)
 					}						
-				fmt.Fprint(w,myform)
+				StructPrt(myform,os.Stdout)
+				StructPrt(myform,w)
 				}
 			})
 				
@@ -64,7 +68,7 @@ func FormCreate(formData *MyForm) (form string, err error){
 	str:="<h1>Hello!</h1> <form method='POST' action='http:\\localhost:1234'>"
 
 //parse struct definition	
-	val := reflect.ValueOf(myform).Elem()
+	val := reflect.ValueOf(formData).Elem()
 	for i := 0; i < val.NumField(); i++ {			
 			tag := val.Type().Field(i).Tag
 			str=str+TagToHtml(tag)			
@@ -82,11 +86,11 @@ func FormCreate(formData *MyForm) (form string, err error){
 					
 					// type select					
 					if field_type == "select" {
-						str = str + SelectTagCreate(field,tag.Get("select"))
+						SelectTagCreate(&str,field,tag.Get("select"))
 				
 					// type radio
 					}else if field_type == "radio"{
-						str = str + RadioTagCreate(field,tag.Get("radio"))
+						RadioTagCreate(&str,field,tag.Get("radio"))
 						
 					// textarea	
 					}else if field_type == "textarea"{
@@ -102,6 +106,18 @@ func FormCreate(formData *MyForm) (form string, err error){
 							}else if tt == "string" {
 								value_field := val.Field(i).String()
 								def_value="value='"+value_field+"'"
+							}else if tt == "float64" {
+								value_field := val.Field(i).Float()
+								def_value="value="+strconv.FormatFloat(value_field,'f',5,64)
+							}else if tt == "uint" || tt == "uint64"{
+								value_field := val.Field(i).Uint()
+								def_value="value="+strconv.FormatUint(value_field,10)
+							}else if tt == "bool"{
+								value_field := val.Field(i).Bool()
+								def_value="value="+strconv.FormatBool(value_field)
+							}else if tt == "time.Duration" {
+								value_field := val.Field(i).Int()
+								def_value="value="+strconv.FormatInt(value_field,10)
 							}else{
 								def_value=""
 							}
@@ -115,6 +131,7 @@ func FormCreate(formData *MyForm) (form string, err error){
 	}
 	str=str+"<button type='submit'>send</button> </form>"
 	form=str
+
 return
 }
 
@@ -122,7 +139,7 @@ return
 func FormRead(formData *MyForm, r *http.Request) (err error){
 	fmt.Println("* POST request ",time.Now())
 	err = nil	
-	val := reflect.ValueOf(myform).Elem()
+	val := reflect.ValueOf(formData).Elem()
 	r.ParseForm()
 	for i := 0; i < val.NumField(); i++ {
 		f := val.Field(i)
@@ -130,17 +147,16 @@ func FormRead(formData *MyForm, r *http.Request) (err error){
 	
 		field := tag.Get("field")
 		form_v := r.PostFormValue(field)		
-		t := val.Type().Field(i).Type.Name()
+		struct_field_type := val.Type().Field(i).Type.Name()
 		var v reflect.Value
 		if (tag.Get("type") == "radio"){
-			fmt.Println("radio: ",form_v)
 			if (form_v == strings.Split(tag.Get("radio"),";")[0]){	
 				v = reflect.ValueOf(true)
 			}else{
 				v = reflect.ValueOf(false)
 			}
 		}else{
-		switch t {
+		switch struct_field_type {
 			case "string":{
 				v = reflect.ValueOf(form_v)}
 			case "int":{
@@ -153,23 +169,24 @@ func FormRead(formData *MyForm, r *http.Request) (err error){
 				form_vf,_:=strconv.ParseFloat(form_v,64)
 				v = reflect.ValueOf(form_vf)}
 			case "uint":{
-				form_vf,_:=strconv.ParseUint(form_v,10,0)
-				v = reflect.ValueOf(form_vf)}
+				form_vui,_:=strconv.ParseUint(form_v,10,0)
+				v = reflect.ValueOf(form_vui)}
 			case "uint64":{
-				form_vf,_:=strconv.ParseUint(form_v,10,64)
-				v = reflect.ValueOf(form_vf)}
-			case "bool":{
-				 
+				form_vui,_:=strconv.ParseUint(form_v,10,64)
+				v = reflect.ValueOf(form_vui)}
+			case "Duration":{
+				form_vt,_:=time.ParseDuration(form_v)
+				v = reflect.ValueOf(form_vt)}			
+			case "bool":{				 
 				form_vb,_:=strconv.ParseBool(form_v)
-				fmt.Println("boolean:" , form_vb,val.Type().Field(i).Name)
 				v = reflect.ValueOf(form_vb)}
 			default:
-				fmt.Println("type not supported!") 
+				fmt.Println("type of",field,"(",struct_field_type,") not supported!") 
 			
 			}
 		}
 		f.Set(v)
-		fmt.Println("myform: ",myform)		
+		fmt.Println("myform,",i," step:",myform)		
 	}
 return
 }
@@ -178,8 +195,8 @@ func TagToHtml(tag reflect.StructTag)(htmlstr string){
 return
 }
 
-func SelectTagCreate(f,tag string)(s string){
-	s ="<select name="+f+">"
+func SelectTagCreate(s *string,f,tag string){
+	*s +="<select name="+f+">"
 	options := strings.Split(tag,",")
 	var temps1 string
 	for _,v := range options{
@@ -187,18 +204,50 @@ func SelectTagCreate(f,tag string)(s string){
 		temps1 = ""
 		if len(one_option)>1 {temps1="selected"}
 		option_values := strings.Split(one_option[0],"=")
-		s = s + "<option value='"+option_values[1]+"' "+temps1+">"+option_values[0]+"</option>"
+		*s += "<option value='"+option_values[1]+"' "+temps1+">"+option_values[0]+"</option>"
 		}
-	s = s+"</select><br>"
+	*s += "</select><br>"
 return
 }
 
-func RadioTagCreate(f,tag string)(s string){
-	s = "<input type='radio' name='"+f+"' "
+func RadioTagCreate(s *string,f,tag string){//(s string){
+	*s += "<input type='radio' name='"+f+"' "
 	radio_value := strings.Split(tag,";")
-	s = s + "value='"+radio_value[0]+"' "
+	*s += "value='"+radio_value[0]+"' "
 	if len(radio_value)>1 {
-		s = s + "checked"}
-	s = s+"><br>"
+		*s += "checked"}
+	*s += "><br>"
 return
+}
+
+func StructPrt(st *MyForm,w io.Writer){
+	var fv string
+	
+	val := reflect.ValueOf(st).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		tt := val.Type().Field(i).Type.String()
+		fname := val.Type().Field(i).Name
+		if tt == "int" || tt == "int64" {
+			value_field := val.Field(i).Int()
+			fv= fname+" : "+strconv.FormatInt(value_field,10)
+		}else if tt == "string" {
+			value_field := val.Field(i).String()
+			fv=fname+" : "+value_field
+		}else if tt == "float64" {
+			value_field := val.Field(i).Float()
+			fv=fname+" : "+strconv.FormatFloat(value_field,'f',5,64)
+		}else if tt == "uint" || tt == "uint64"{
+			value_field := val.Field(i).Uint()
+			fv=fname+" : "+strconv.FormatUint(value_field,10)
+		}else if tt == "bool"{
+			value_field := val.Field(i).Bool()
+			fv=fname+" : "+strconv.FormatBool(value_field)
+		}else if tt == "time.Duration" {
+			value_field := val.Field(i).Int()
+			fv=fname+" : "+strconv.FormatInt(value_field,10)
+		}else{
+			fv=fname+" : "+" unknown type"
+		}
+	fmt.Fprintln(w,fv)
+	}
 }
